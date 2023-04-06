e4c537bd5b647e7038f6ae33af8cde1ef07e06dd
erica.b.graboyes <erica.b.graboyes@gmail.com>
2023-04-05 20:08:16 -0500

    simplify bar chart to sort property vs. array

--- analysis/app.js
+++ analysis/app.js
@@ -84,36 +84,27 @@ function populateGauge(filterMetaData) {
 function populateCharts() {
     // assign filteredSamples to testSubjectID
     filteredSamples = selectedData.filter(person => person.id === testSubject)[0];
-    // console.log("SampleValues:",filteredSamples);
+    console.log("SampleValues:",filteredSamples.id);
 
     // assign otu global variables to filteredSamples.properties
     otuIDs = filteredSamples.otu_ids;
-    // console.log("otuID:", otuIDs);
+    console.log("otuID:", otuIDs);
 
     otuLabels = filteredSamples.otu_labels;
     // console.log("otuLabels:", otuLabels);
 
     otuValues = filteredSamples.sample_values;
-    // console.log("outValues:", otuValues);
-
-    // create object for each sample id, value and lable, push to array
-    // needed to ensure lables and otuID are assigned to proper values when sorted
-    for (var d=0; d<otuIDs.length; d++) {
-        dataToChart.push({
-            id:otuIDs[d],
-            label:otuLabels[d],
-            value:otuValues[d]})};
-    // console.log("testchart:", dataToChart);
+    console.log("outValues:", otuValues);
 
     // declare trace for bubblechart
     var bubble = [{
-        x: dataToChart.map(dTC => dTC.id),
-        y: dataToChart.map(dTC => dTC.value),
-        text: dataToChart.map(dTC => dTC.label),
+        x: otuIDs,
+        y: otuValues,
+        text: otuLabels,
         mode: 'markers',
         marker: {
-            size: dataToChart.map(dTC => dTC.value),
-            color: dataToChart.map(dTC => dTC.id),
+            size: otuValues,
+            color: otuIDs,
             //colorscale: 'Earth',
             opacity: 0.8},
         },];
@@ -153,17 +144,11 @@ function populateCharts() {
             },
         },};
 
-    // sort chart data, assign to global variable
-    sortChartData = dataToChart.sort((d1, d2) => {
-        return d2.value - d1.value;}
-        );
-    // console.log("sortedData:", sortChartData);
-
     // declare trace for barGraph
     var barGraph = [{
-        x: (sortChartData.map(sCD => sCD.value)).slice(0,10).reverse(),
-        y: (sortChartData.map(sCD => 'OTU '+ sCD.id)).slice(0,10).reverse(),
-        text: (sortChartData.map(sCD => sCD.label)).slice(0,10).reverse(),
+        x: otuValues.slice(0,10).reverse(),
+        y: otuIDs.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+        text: otuLabels.slice(0,10).reverse(),
         hoverlabel: {font: {size:12}},
         marker: {color:  ['#ECEFF1', '#CFD8DC', '#B0BEC5', '#90A4AE', '#78909C', '#607D8B','#546E7A','#455A64','#37474F', '#263238'],},
         type: "bar",
@@ -183,7 +168,6 @@ function populateCharts() {
     Plotly.newPlot('bubble', bubble, bubbleLayout)
 };
 
-
 // function to read data, populate initial graphs
 function init() {
     // use d3 to select drop down, assign to variable
@@ -194,7 +178,6 @@ function init() {
         rawData = data
         dropDownNames = data.names;
         metaData = data.metadata;
-        console.log("testmetadata",metaData[0])
         testSubject = data.metadata[0].id.toString()
         selectedData = data.samples;
 
